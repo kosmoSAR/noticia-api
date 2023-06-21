dca6d0f1b43de041bf3bd18510f0013dcb36f055
kosmoSAR <santiagoaristizabal.kosmetikon@gmail.com>
2023-06-21 15:01:14 -0500

    finalización del cap 17

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent {
+  @Input() titulo:string;
+
+  constructor(){
+    this.titulo="";
+  }
 
 }
